80d4a242cf9e28f06287b2b6be23ce0e9ea4757a
Samuelj-santos <samuel.00000847429@unicap.br>
2023-09-21 18:38:11 -0300

    Update

---
+++ LSE/LSEInt.java
@@ -0,0 +1,64 @@
+public class LSEInt {
+    private LSENode primeiro;
+
+    public void exibirTodos() {
+        LSENode aux;
+        if (this.isEmpty() == true) {
+            System.out.println("Lista vazia!");
+        
+        }else {
+            aux = this.primeiro;
+            while(aux != null) {
+                System.out.println(aux.getInfo());
+                aux = aux.getProx();
+            }            
+        }   
+        }
+        public boolean isEmpty () {
+            if (this.primeiro == null) {
+                return true;
+            }else {
+                return false;
+            }
+        }
+            public void inserirNoInicio (Integer valor) {
+                LSENode novo;
+                novo = new LSENode (valor);
+                if (this.isEmpty() == true) {
+
+                   this.primeiro = novo;
+                  }else {
+                    novo.setProx(this.primeiro);
+                      this.primeiro = novo;
+                 }
+                 System.out.println("Inserção efetuada.");
+                
+                 
+                
+                
+                }
+             public void removerComeço(){
+              if (isEmpty() == true) {
+                System.out.println("A lista esta vazia");
+              }else{
+                this.primeiro = this.primeiro.getProx();
+              }
+             }
+
+             public void procurarValor(Integer valor){
+                LSENode aux;
+               if (isEmpty() == true) {
+                System.out.println("A lista esta vazia");
+               }else{
+                int cont = 0;
+                aux = this.primeiro;
+                 while (aux != null) {
+                    if (aux.getInfo() == valor) {
+                        cont++;
+                    }
+                    aux = aux.getProx();
+                 }
+                 System.out.println("O valor " + valor+" apareceu " + cont+" vezes");
+               }
+             }
+            }
---
+++ LSE/ListasDeInteiros.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+public class ListasDeInteiros {
+    public static void main(String[] args) {
+        Scanner in = new Scanner (System.in);
+        LSEInt numeros;
+        numeros = new LSEInt();
+        int op, num;
+        do {
+            exibirOpcoes();
+            op = in.nextInt();
+            switch (op) {
+                case 1: 
+                System.out.print("Informe o valor a ser inserido: ");
+            num = in.nextInt();
+            numeros.inserirNoInicio(num);
+            break;
+            case 2:
+             numeros.exibirTodos();
+            break;
+            case 3:
+            numeros.removerComeço();
+            break;
+            case 4:
+            System.out.println("Insira o valor que deseja procurar");
+            Integer valor = in.nextInt();
+            numeros.procurarValor(valor);
+            break;
+            case 0: System.out.println("Bye bye");
+            break;
+            default: System.out.println("Opção inválida!");}
+                  } while (op != 0);
+                }
+                public static void exibirOpcoes() {
+                    System.out.println("Opções");
+                    System.out.println("1 -Inserir novo valor no início da lista");
+                    System.out.println("2 -Exibir valores na lista");
+                    System.out.println("3 -Remover o primeiro valor da lista");
+                    System.out.println("4 -Procura valor,retorna quantas vezes ele aparece na lista");
+                    System.out.println("0 -Encerrar programa");
+                    System.out.print("Digite a opção desejada: ");
+                }}
